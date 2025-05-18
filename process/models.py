760375188691af2e7c5945e6bb8d9d1c9fb64ad3
park <rudwls2717@pusan.ac.kr>
2025-05-18 16:03:51 +0900

    hide_GnS

--- process/models.py
+++ process/models.py
@@ -27,114 +27,114 @@ from torch_geometric.nn import GCNConv, global_mean_pool as gep
 ########## A2H-Net
 ########################################################################################################################
 
-class GnS(torch.nn.Module):
-    def __init__(self, n_output=1, num_features_xd=55, num_features_xt=25, n_filters=32, embed_dim=128,
-                 output_dim=128, dropout=0.2, joint='concat'):
+# class GnS(torch.nn.Module):
+#     def __init__(self, n_output=1, num_features_xd=55, num_features_xt=25, n_filters=32, embed_dim=128,
+#                  output_dim=128, dropout=0.2, joint='concat'):
 
-        super(GnS, self).__init__()
+#         super(GnS, self).__init__()
 
-        dim = 32
-        jdim = 256
-        self.joint = joint
-        if self.joint in ['add', 'multiple']:
-            jdim = 128
+#         dim = 32
+#         jdim = 256
+#         self.joint = joint
+#         if self.joint in ['add', 'multiple']:
+#             jdim = 128
 
-        # GIN layers (drug)
-        nn1_xd = Sequential(Linear(num_features_xd, dim), ReLU(), Linear(dim, dim))
-        self.conv1_xd = GINConv(nn1_xd)
-        self.bn1_xd = torch.nn.BatchNorm1d(dim)
+#         # GIN layers (drug)
+#         nn1_xd = Sequential(Linear(num_features_xd, dim), ReLU(), Linear(dim, dim))
+#         self.conv1_xd = GINConv(nn1_xd)
+#         self.bn1_xd = torch.nn.BatchNorm1d(dim)
 
-        nn2_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
-        self.conv2_xd = GINConv(nn2_xd)
-        self.bn2_xd = torch.nn.BatchNorm1d(dim)
+#         nn2_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
+#         self.conv2_xd = GINConv(nn2_xd)
+#         self.bn2_xd = torch.nn.BatchNorm1d(dim)
 
-        nn3_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
-        self.conv3_xd = GINConv(nn3_xd)
-        self.bn3_xd = torch.nn.BatchNorm1d(dim)
+#         nn3_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
+#         self.conv3_xd = GINConv(nn3_xd)
+#         self.bn3_xd = torch.nn.BatchNorm1d(dim)
 
-        nn4_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
-        self.conv4_xd = GINConv(nn4_xd)
-        self.bn4_xd = torch.nn.BatchNorm1d(dim)
+#         nn4_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
+#         self.conv4_xd = GINConv(nn4_xd)
+#         self.bn4_xd = torch.nn.BatchNorm1d(dim)
 
-        # 1D convolution on protein sequence
-        self.embedding_xt = nn.Embedding(num_features_xt + 1, embed_dim)
-        self.conv_xt_1 = nn.Conv1d(in_channels=1000, out_channels=n_filters, kernel_size=8)  # batch, 32, 121
+#         # 1D convolution on protein sequence
+#         self.embedding_xt = nn.Embedding(num_features_xt + 1, embed_dim)
+#         self.conv_xt_1 = nn.Conv1d(in_channels=1000, out_channels=n_filters, kernel_size=8)  # batch, 32, 121
 
-        if 'att' in self.joint:
-            nn5_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, 121))
-            self.conv5_xd = GINConv(nn5_xd) # batch, node, 121
-            self.bn5_xd = torch.nn.BatchNorm1d(121)
+#         if 'att' in self.joint:
+#             nn5_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, 121))
+#             self.conv5_xd = GINConv(nn5_xd) # batch, node, 121
+#             self.bn5_xd = torch.nn.BatchNorm1d(121)
 
-            if self.joint == 'bi_att':
-                self.jc = weight_norm(BANLayer(v_dim=121, q_dim=121, h_dim=jdim, h_out=2), name='h_mat', dim=None)
-            elif self.joint == 'cross_att':
-                self.jc = CrossAttention(290, n_filters, embed_dim=121, out_dim=jdim, num_heads=8)
-            elif self.joint == 'co_att':
-                self.jc = CoAttention(290, n_filters, embed_dim=121, out_dim=jdim, num_heads=8)
-            else:
-                raise Exception('wrong att type')
-        else:
-            nn5_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
-            self.conv5_xd = GINConv(nn5_xd)
-            self.bn5_xd = torch.nn.BatchNorm1d(dim)
+#             if self.joint == 'bi_att':
+#                 self.jc = weight_norm(BANLayer(v_dim=121, q_dim=121, h_dim=jdim, h_out=2), name='h_mat', dim=None)
+#             elif self.joint == 'cross_att':
+#                 self.jc = CrossAttention(290, n_filters, embed_dim=121, out_dim=jdim, num_heads=8)
+#             elif self.joint == 'co_att':
+#                 self.jc = CoAttention(290, n_filters, embed_dim=121, out_dim=jdim, num_heads=8)
+#             else:
+#                 raise Exception('wrong att type')
+#         else:
+#             nn5_xd = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
+#             self.conv5_xd = GINConv(nn5_xd)
+#             self.bn5_xd = torch.nn.BatchNorm1d(dim)
 
-            self.fc1_xd = Linear(dim, output_dim)
-            self.fc1_xt = nn.Linear(32 * 121, output_dim)
+#             self.fc1_xd = Linear(dim, output_dim)
+#             self.fc1_xt = nn.Linear(32 * 121, output_dim)
 
-            if self.joint in ['concat', 'add', 'multiple']:
-                self.jc = Simple_Joint(self.joint)
-            elif self.joint == 'bi':
-                self.jc = Bilinear_Joint(output_dim, jdim)
-            else:
-                raise Exception(f'{self.joint} method not supported!!!')
+#             if self.joint in ['concat', 'add', 'multiple']:
+#                 self.jc = Simple_Joint(self.joint)
+#             elif self.joint == 'bi':
+#                 self.jc = Bilinear_Joint(output_dim, jdim)
+#             else:
+#                 raise Exception(f'{self.joint} method not supported!!!')
 
-        # dense
-        self.classifier = nn.Sequential(
-            nn.Linear(jdim, 1024),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(1024, 256),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(256, n_output),
-        )
+#         # dense
+#         self.classifier = nn.Sequential(
+#             nn.Linear(jdim, 1024),
+#             nn.ReLU(),
+#             nn.Dropout(dropout),
+#             nn.Linear(1024, 256),
+#             nn.ReLU(),
+#             nn.Dropout(dropout),
+#             nn.Linear(256, n_output),
+#         )
 
-    def forward(self, data):
-        drug, target, y = data
-        xd, xd_ei, xd_batch = drug.x, drug.edge_index, drug.batch
-        xt = target.x
+#     def forward(self, data):
+#         drug, target, y = data
+#         xd, xd_ei, xd_batch = drug.x, drug.edge_index, drug.batch
+#         xt = target.x
 
-        # drug
-        xd = F.relu(self.conv1_xd(xd, xd_ei))
-        xd = self.bn1_xd(xd)
-        xd = F.relu(self.conv2_xd(xd, xd_ei))
-        xd = self.bn2_xd(xd)
-        xd = F.relu(self.conv3_xd(xd, xd_ei))
-        xd = self.bn3_xd(xd)
-        xd = F.relu(self.conv4_xd(xd, xd_ei))
-        xd = self.bn4_xd(xd)
-        xd = F.relu(self.conv5_xd(xd, xd_ei))
-        xd = self.bn5_xd(xd)
+#         # drug
+#         xd = F.relu(self.conv1_xd(xd, xd_ei))
+#         xd = self.bn1_xd(xd)
+#         xd = F.relu(self.conv2_xd(xd, xd_ei))
+#         xd = self.bn2_xd(xd)
+#         xd = F.relu(self.conv3_xd(xd, xd_ei))
+#         xd = self.bn3_xd(xd)
+#         xd = F.relu(self.conv4_xd(xd, xd_ei))
+#         xd = self.bn4_xd(xd)
+#         xd = F.relu(self.conv5_xd(xd, xd_ei))
+#         xd = self.bn5_xd(xd)
 
-        embedded_xt = self.embedding_xt(xt)
-        conv_xt = self.conv_xt_1(embedded_xt)
+#         embedded_xt = self.embedding_xt(xt)
+#         conv_xt = self.conv_xt_1(embedded_xt)
 
-        # joint
-        if 'att' in self.joint:
-            xd = xd.view(len(y), 290, 121)
-            xj = self.jc(xd, conv_xt)
+#         # joint
+#         if 'att' in self.joint:
+#             xd = xd.view(len(y), 290, 121)
+#             xj = self.jc(xd, conv_xt)
 
-        else:
-            # flatten
-            xd = gap(xd, xd_batch)
-            xd = F.relu(self.fc1_xd(xd))
-            xd = F.dropout(xd, p=0.2, training=self.training)
-            xt = self.fc1_xt(conv_xt.view(-1, 32 * 121))
-            xj = self.jc(xd, xt)
+#         else:
+#             # flatten
+#             xd = gap(xd, xd_batch)
+#             xd = F.relu(self.fc1_xd(xd))
+#             xd = F.dropout(xd, p=0.2, training=self.training)
+#             xt = self.fc1_xt(conv_xt.view(-1, 32 * 121))
+#             xj = self.jc(xd, xt)
 
-        # dense
-        out = self.classifier(xj).squeeze(1)
-        return out, y
+#         # dense
+#         out = self.classifier(xj).squeeze(1)
+#         return out, y
 
 ########################################################################################################################
 ########## GraphDTA
